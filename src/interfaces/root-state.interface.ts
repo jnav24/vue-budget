567bccc1306ec78e15fc4fcb82fea719213adff2
Justin Navarro <info@justinnavarro.net>
2019-01-27 16:20:54 -0500

    update the root state

--- src/interfaces/root-state.interface.ts
+++ src/interfaces/root-state.interface.ts
@@ -1,8 +1,9 @@
 import {UserStateInterface} from '@/interfaces/user-state.interface';
 import {BillsStateInterface} from '@/interfaces/bills-state.interface';
+import {BudgetStateInterface} from '@/interfaces/budget-state.interface';
 
 export interface RootStateInterface {
     Bills: BillsStateInterface;
-    Budget: any;
+    Budget: BudgetStateInterface;
     User: UserStateInterface;
 }
