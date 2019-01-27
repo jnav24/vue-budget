805d8b10f5e4c88c9a55899bc36302738bcf1d3f
Justin Navarro <info@justinnavarro.net>
2019-01-27 13:58:55 -0500

    add some copy to the template page

--- src/pages/dashboard/budget/template/Template.vue
+++ src/pages/dashboard/budget/template/Template.vue
@@ -5,7 +5,13 @@
 	<div class="page budget-template">
 		<v-layout justify-center>
 			<v-flex lg8 xl8>
-				<h1 class="header__h1">Budget Template</h1>
+				<v-layout align-center style="margin-bottom: 25px;">
+					<v-flex>
+						<h1 class="header__h1">Budget Template</h1>
+						<p>You can add all your bank, credit cards and other monthly expenses, you would like to track on this page. Everything entered on this page, will show when you add a new monthly budget.</p>
+						<p>If you were to delete an item, it will still show on your already existing budgets but it will not show on new budgets.</p>
+					</v-flex>
+				</v-layout>
 
 				<v-card>
 					<v-card-title>
