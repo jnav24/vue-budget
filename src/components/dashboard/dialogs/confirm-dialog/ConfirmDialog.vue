92fdf952f4094214c43dbaa079098a68a6920bd9
Justin Navarro <info@justinnavarro.net>
2019-01-26 20:25:57 -0500

    styled the confirm dialog

--- src/components/dashboard/dialogs/confirm-dialog/ConfirmDialog.vue
+++ src/components/dashboard/dialogs/confirm-dialog/ConfirmDialog.vue
@@ -1,14 +1,27 @@
 <script src="./confirm-dialog.ts" lang="ts"></script>
+<style src="./confirm-dialog.scss" lang="scss" module></style>
 
 <template>
 	<v-dialog
-		class="confirm-dialog dialogs"
+		:class="[$style['confirm-dialog'], $style.dialogs]"
 		max-width="500px"
 		v-model="showDialog">
-		<v-card>
+		<v-card :class="[$style['confirm-card']]">
 			<v-card-text>
-				This is a confirmation box
+				<v-icon :class="$style['warning-icon']">error_outline</v-icon>
+				<p :class="$style['confirm-text']">Are you sure?</p>
 			</v-card-text>
+
+			<v-card-actions>
+				<v-layout justify-center>
+					<v-btn
+						@click="closeDialog()">Cancel</v-btn>
+					<v-btn
+						@click="closeDialog()"
+						color="danger"
+						dark>Confirm</v-btn>
+				</v-layout>
+			</v-card-actions>
 		</v-card>
 	</v-dialog>
 </template>
