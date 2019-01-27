aafa7241bb4cf5772d87770f63009d0414e5c151
Justin Navarro <info@justinnavarro.net>
2019-01-27 16:00:23 -0500

    updated user store with explicit typing

--- src/interfaces/root-state.interface.ts
+++ src/interfaces/root-state.interface.ts
@@ -1,5 +1,7 @@
+import {UserStateInterface} from '@/interfaces/user-state.interface';
+
 export interface RootStateInterface {
     Bills: any;
     Budget: any;
-    User: any;
+    User: UserStateInterface;
 }
