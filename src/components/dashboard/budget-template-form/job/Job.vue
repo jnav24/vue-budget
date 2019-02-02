8bbe1b7a203cf0d498b4f31485736a4c42280479
Justin Navarro <info@justinnavarro.net>
2019-02-01 21:57:11 -0500

    created job component

--- src/components/dashboard/budget-template-form/job/Job.vue
+++ src/components/dashboard/budget-template-form/job/Job.vue
@@ -3,6 +3,62 @@
 
 <template>
 	<div class="budget-template job">
-		Job component
+		<BudgetTemplateComponent
+			:reset="resetForm"
+			@buttonClicked="validateForm($event)">
+			<v-layout>
+				<v-flex>
+					<v-text-field
+						v-model="form.name.value"
+						:rules="form.name.rules"
+						label="Name"></v-text-field>
+				</v-flex>
+
+				<v-spacer></v-spacer>
+
+				<v-flex>
+					<v-text-field
+						v-model="form.amount.value"
+						:rules="form.amount.rules"
+						label="Pay Amount"></v-text-field>
+				</v-flex>
+			</v-layout>
+
+			<v-layout>
+				<v-flex>
+					<v-select
+						v-model="form.pay_period.value"
+						:rules="form.pay_period.rules"
+						:items="payPeriods"
+						item-value="value"
+						item-text="text"
+						label="Pay Period"></v-select>
+				</v-flex>
+
+				<v-spacer></v-spacer>
+
+				<v-flex>
+					<v-menu
+						:close-on-content-click="false"
+						v-model="picker"
+						:nudge-right="40"
+						lazy
+						transition="scale-transition"
+						offset-y
+						full-width
+						min-width="290px">
+						<v-text-field
+							slot="activator"
+							v-model="form.initial_pay_date.value"
+							label="Next Paydate"
+							prepend-icon="event"
+							readonly></v-text-field>
+						<v-date-picker
+							v-model="form.initial_pay_date.value"
+							@input="picker = false"></v-date-picker>
+					</v-menu>
+				</v-flex>
+			</v-layout>
+		</BudgetTemplateComponent>
 	</div>
 </template>
--- src/components/dashboard/budget-template-form/job/job.ts
+++ src/components/dashboard/budget-template-form/job/job.ts
@@ -1,6 +1,73 @@
-import { Vue, Component } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+import BudgetTemplateForm from '@/components/dashboard/budget-template-form/budget-template-form';
+import {BudgetTemplateFormInterface} from '@/components/dashboard/budget-template-form/budget-template-form.interface';
+import {BudgetListAddInterface} from '@/interfaces/buget-list-add.interface';
+import {FormInterface} from '@/interfaces/form.interface';
+import {timestampService} from '@/module';
+import BudgetTemplateComponent from '@/components/dashboard/budget-template-form/BudgetTemplateForm.vue';
 
-@Component
-class Job extends Vue {}
+@Component({
+    components: {
+        BudgetTemplateComponent,
+    },
+})
+class Job extends BudgetTemplateForm implements BudgetTemplateFormInterface {
+    public form: FormInterface = {
+        name: {
+            value: '',
+            rules: [
+                (v: any) => !!v || 'Name is required',
+            ],
+        },
+        amount: {
+            value: '',
+            rules: [],
+        },
+        pay_period: {
+            value: '',
+            rules: [],
+        },
+        initial_pay_date: {
+            value: '',
+            rules: [],
+        },
+    };
+    public payPeriods: any = [
+        { value: '1', text: 'Weekly' },
+        { value: '2', text: 'Bi-Weekly' },
+        { value: '3', text: 'Semi-Monthly' },
+        { value: '4', text: 'Monthly' },
+    ];
+    public picker: boolean = false;
+
+    public setupForm() {
+        if (typeof this.data !== 'undefined' && Object.keys(this.data).length) {
+            this.form.name.value = this.data.name;
+            this.form.amount.value = this.data.amount;
+            this.form.pay_period.value = this.data.pay_period;
+            this.form.initial_pay_date = this.data.initial_pay_date;
+        } else {
+            this.form.initial_pay_date.value = timestampService.format('', 'YYYY-MM-DD');
+        }
+    }
+
+    public setData(): BudgetListAddInterface {
+        return {
+            type: 'job',
+            data: {
+                id: 'temp_' + timestampService.generateUnixId(),
+                name: this.form.name.value,
+                amount: this.form.amount.value,
+                pay_period: this.form.pay_period.value,
+                initial_pay_date: this.form.initial_pay_date.value,
+            },
+        };
+    }
+
+    protected validateForm(obj: { valid: boolean }) {
+        this.templateValid = obj.valid;
+        this.submit(this.setData());
+    }
+}
 
 export default Job;
