c06a2576b520b777def1b6855640c3892b822e81
Justin Navarro <info@justinnavarro.net>
2019-02-06 16:31:54 -0500

    created alert dialog

---
+++ src/components/dashboard/dialogs/alert-dialog/AlertDialog.vue
@@ -0,0 +1,13 @@
+<script src="./alert-dialog.ts" lang="ts"></script>
+<style src="./alert-dialog.scss" lang="scss" module></style>
+
+<template>
+	<v-dialog
+		:class="[$style['alert-dialog'], $style.dialogs]"
+		max-width="500px"
+		v-model="showDialog">
+		<v-card :class="[$style['confirm-card']]">
+			<v-card-text>Hello</v-card-text>
+		</v-card>
+	</v-dialog>
+</template>
---
+++ src/components/dashboard/dialogs/alert-dialog/alert-dialog.ts
@@ -0,0 +1,7 @@
+import { Component } from 'vue-property-decorator';
+import Dialogs from '@/components/dashboard/dialogs/dialogs';
+
+@Component
+class AlertDialog extends Dialogs {}
+
+export default AlertDialog;
