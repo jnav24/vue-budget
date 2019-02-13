ef82d810b5965b8054316b0f1442ef8c893c4b10
Justin Navarro <info@justinnavarro.net>
2019-02-13 07:57:05 -0500

    created budget credit card component

---
+++ src/components/dashboard/budgets/credit-cards/CreditCards.vue
@@ -0,0 +1,8 @@
+<script src="./credit-cards.ts" lang="ts"></script>
+<style src="./credit-cards.scss" lang="scss" scoped></style>
+
+<template>
+	<div class="credit-card budget">
+		cc...
+	</div>
+</template>
---
+++ src/components/dashboard/budgets/credit-cards/credit-cards.ts
@@ -0,0 +1,7 @@
+import { Component } from 'vue-property-decorator';
+import Budgets from '@/components/dashboard/budgets/budgets';
+
+@Component
+class CreditCard extends Budgets {}
+
+export default CreditCard;
