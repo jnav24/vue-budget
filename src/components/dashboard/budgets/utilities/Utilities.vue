a9066937aec808d2fb7a73da7cd6d70fc637d15b
Justin Navarro <info@justinnavarro.net>
2019-02-15 14:38:08 -0500

    finished building the utilities component

--- src/components/dashboard/budgets/utilities/Utilities.vue
+++ src/components/dashboard/budgets/utilities/Utilities.vue
@@ -8,6 +8,33 @@
 			class="budget__row"
 			v-for="(item, index) in budgetData"
 			:key="item.id">
+			<v-flex lg1 xl1>
+				<v-icon v-if="isBillPaid(item)" class="budget__paid-icon">check</v-icon>
+			</v-flex>
+
+			<v-flex lg4 xl4>
+				<h3 class="budget__title">{{ item.name }}</h3>
+				<p class="budget__type">{{ getType(item.utility_type_id) }}</p>
+			</v-flex>
+
+			<v-flex lg3 xl3>
+				<p class="budget__price price-danger price-tac">{{ getDollarAmount(item.amount) }}</p>
+			</v-flex>
+
+			<v-flex lg2 xl2>
+				<p class="budget__due" v-if="!isBillPaid(item)">Due {{ getDueDate(item.due_date) }}</p>
+				<p class="budget__paid" v-if="isBillPaid(item)">Paid {{ getDueDate(item.due_date) }}</p>
+			</v-flex>
+
+			<v-flex lg2 xl2>
+				<v-layout justify-end>
+					<v-btn
+						fab
+						class="btn--details__icon">
+						<v-icon>edit</v-icon>
+					</v-btn>
+				</v-layout>
+			</v-flex>
 		</v-layout>
 	</div>
 </template>
