61b3b12bc975a6310911cbd410656885e25f8965
Justin Navarro <info@justinnavarro.net>
2019-02-27 20:45:46 -0500

    created vehicle interface and template form

--- src/components/dashboard/budget-template-form/vehicle/vehicle.ts
+++ src/components/dashboard/budget-template-form/vehicle/vehicle.ts
@@ -3,21 +3,48 @@ import BudgetTemplateForm from '@/components/dashboard/budget-template-form/budg
 import {BudgetTemplateFormInterface} from '@/components/dashboard/budget-template-form/budget-template-form.interface';
 import {FormInterface} from '@/interfaces/form.interface';
 import {BudgetListAddInterface} from '@/interfaces/buget-list-add.interface';
+import {VehicleInterface} from '@/interfaces/vehicle.interface';
 
 @Component
 class Vehicle extends BudgetTemplateForm implements BudgetTemplateFormInterface {
-    protected templateForm: FormInterface = {};
+    protected templateForm: FormInterface = {
+        make: {
+            value: '',
+            rules: [],
+        },
+        model: {
+            value: '',
+            rules: [],
+        },
+        year: {
+            value: '',
+            rules: [],
+        },
+        mileage: {
+            value: '',
+            rules: [],
+        },
+    };
 
     public setData(): BudgetListAddInterface {
-        return {} as BudgetListAddInterface;
+        const data: VehicleInterface = {
+            make: this.form.make.value,
+            model: this.form.model.value,
+            year: this.form.year.value,
+            mileage: this.form.mileage.value,
+        };
+
+        return this.setDataForSaving(data, 'vehicles');
     }
 
     public setupForm() {
-        // ...
-    }
-
-    public validateForm(obj: { valid: boolean }) {
-        // ...
+        if (typeof this.data !== 'undefined' && Object.keys(this.data).length) {
+            this.editMode = true;
+            this.form.make.value = this.data.make;
+            this.form.model.value = this.data.model;
+            this.form.year.value = this.data.value;
+            this.form.mileage.value = this.data.milage;
+        }
     }
 }
 
---
+++ src/interfaces/vehicle.interface.ts
@@ -0,0 +1,7 @@
+export interface VehicleInterface {
+    id?: number | string;
+    make: string;
+    model: string;
+    year: string;
+    mileage: string;
+}
