fdcda4da21c2a91a7e1225577f5026a649335ab4
Justin Navarro <info@justinnavarro.net>
2019-03-11 14:32:49 -0400

    add controls state to root state

--- src/interfaces/root-state.interface.ts
+++ src/interfaces/root-state.interface.ts
@@ -3,11 +3,13 @@ import {BudgetStateInterface} from '@/interfaces/budget-state.interface';
 import {TypesStateInterface} from '@/interfaces/types-state.interface';
 import {BudgetTemplateStateInterface} from '@/interfaces/budget-template-state.interface';
 import {AggregationStateInterface} from '@/interfaces/aggregation-state.interface';
+import {ControlsStateInterface} from '@/interfaces/controls-state.interface';
 
 export interface RootStateInterface {
     Aggregation: AggregationStateInterface;
     Budget: BudgetStateInterface;
     BudgetTemplates: BudgetTemplateStateInterface;
+    Controls: ControlsStateInterface;
     Types: TypesStateInterface;
     User: UserStateInterface;
 }
