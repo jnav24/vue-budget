a83e284073b6a6bdb6665fed5a9d470916b3db3e
Justin Navarro <info@justinnavarro.net>
2019-03-26 00:08:55 -0400

    set up the form design

--- src/components/dashboard/profile/user-profile/UserProfile.vue
+++ src/components/dashboard/profile/user-profile/UserProfile.vue
@@ -3,6 +3,32 @@
 
 <template>
 	<div class="profile user-profile">
-		from user profile component
+		<h3 class="header__h3 mt20">Basic Details</h3>
+
+		<v-layout>
+			<v-flex lg5 xl5>
+				<v-text-field
+					label="First Name"></v-text-field>
+			</v-flex>
+
+			<v-spacer></v-spacer>
+
+			<v-flex lg5 xl5>
+				<v-text-field
+					label="Last Name"></v-text-field>
+			</v-flex>
+		</v-layout>
+
+		<v-layout>
+			<v-flex lg5 xl5>
+				<v-text-field
+					disabled
+					label="Email"></v-text-field>
+			</v-flex>
+		</v-layout>
+
+		<v-divider style="margin: 30px 0;"></v-divider>
+
+		<h2 class="header__h3">Vehicles</h2>
 	</div>
 </template>
