451cf4b8a186eb8ae1d3607d0a2fec29b7e6db69
Justin Navarro <info@justinnavarro.net>
2019-03-31 22:55:16 -0400

    created vehicle budget layout

--- src/components/dashboard/budgets/vehicles/Vehicles.vue
+++ src/components/dashboard/budgets/vehicles/Vehicles.vue
@@ -3,8 +3,39 @@
 
 <template>
 	<div class="budget vehicles">
-		<v-layout>
-			hello
+		<v-layout
+			align-center
+			class="budget__row"
+			v-for="(item, index) in budgetData"
+			:key="item.id">
+			<v-flex lg1 xl1>
+				<v-icon v-if="isBillPaid(item)" class="budget__paid-icon">check</v-icon>
+			</v-flex>
+
+			<v-flex lg4 xl4>
+				<h3 class="budget__title">{{ getVehicle(item.user_vehicle_id) }}</h3>
+				<p class="budget__type">{{ getType(item.vehicle_type_id) }}</p>
+			</v-flex>
+
+			<v-flex lg3 xl3>
+				<p class="budget__price price-danger price-tac">{{ getDollarAmount(item.amount) }}</p>
+			</v-flex>
+
+			<v-flex lg2 xl2>
+				<p class="budget__due" v-if="!isBillPaid(item)">Due {{ getDueDate(item.due_date) }}</p>
+				<p class="budget__paid" v-if="isBillPaid(item)">Paid {{ setPaidDate(item.paid_date) }}</p>
+			</v-flex>
+
+			<v-flex lg2 xl2>
+				<v-layout justify-end>
+					<v-btn
+						fab
+						@click="openEditBudget({ type: typeId, data: item })"
+						class="btn--details__icon">
+						<v-icon>edit</v-icon>
+					</v-btn>
+				</v-layout>
+			</v-flex>
 		</v-layout>
 	</div>
 </template>
--- src/components/dashboard/budgets/vehicles/vehicles.ts
+++ src/components/dashboard/budgets/vehicles/vehicles.ts
@@ -1,10 +1,30 @@
 import { Component } from 'vue-property-decorator';
 import Budgets from '@/components/dashboard/budgets/budgets';
+import {RootStateInterface} from '@/interfaces/root-state.interface';
+import {State} from 'vuex-class';
+import {UserStateInterface} from '@/interfaces/user-state.interface';
+import {UserVehicleInterface} from '@/interfaces/user-vehicle.interface';
 
 @Component
 export default class Vehicles extends Budgets {
+    @State((state: RootStateInterface) => state.User) public userState: UserStateInterface;
+
     public mounted() {
         this.type = 'vehicles';
         this.getTypeId();
+    }
+
+    public get vehicles() {
+        return this.userState.vehicles;
+    }
+
+    public getVehicle(id: number): string {
+        const index = this.vehicles.findIndex((obj: UserVehicleInterface) => obj.id === id);
+
+        if (index > -1) {
+            return `${this.vehicles[index].year} ${this.vehicles[index].make} ${this.vehicles[index].model}`;
+        }
+
+        return 'Vehicle not found';
     }
 }
