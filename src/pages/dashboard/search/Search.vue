f54eb2c8754cafbccef5fa451fc89814b8d785f4
Justin Navarro <info@justinnavarro.net>
2019-12-16 12:48:57 -0500

    search page build out

--- src/pages/dashboard/search/Search.vue
+++ src/pages/dashboard/search/Search.vue
@@ -2,7 +2,22 @@
 <style src="./search.scss" lang="scss" scoped></style>
 
 <template>
-	<div id="search">
-		Searching...
+	<div id="search" class="page">
+		<v-layout justify-center>
+			<v-flex lg8 xl8>
+				<v-layout align-center style="margin-bottom: 25px;">
+					<v-flex>
+						<h1 class="header__h1">Search</h1>
+						<p>List of monthly budget. Add, update, or remove budgets and you can go to the budget template on this page.</p>
+					</v-flex>
+				</v-layout>
+
+				<v-card>
+					<v-card-text>
+						Searching....
+					</v-card-text>
+				</v-card>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
