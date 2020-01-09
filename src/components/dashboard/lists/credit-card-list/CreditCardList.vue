320d7a13c95fc38e96349443373e3b4c8b557f99
Justin Navarro <info@justinnavarro.net>
2020-01-09 13:12:09 -0500

    add credit card list

---
+++ src/components/dashboard/lists/credit-card-list/CreditCardList.vue
@@ -0,0 +1,8 @@
+<script src="./credit-card-list.ts" lang="ts"></script>
+<style src="./credit-card-list.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="credit-card-list">
+
+	</div>
+</template>
