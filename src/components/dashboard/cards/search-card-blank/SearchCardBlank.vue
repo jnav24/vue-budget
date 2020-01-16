0b1895076be2ef2c9d6997564abf6a15b447c772
Justin Navarro <info@justinnavarro.net>
2020-01-16 07:01:39 -0500

    created blank search card

---
+++ src/components/dashboard/cards/search-card-blank/SearchCardBlank.vue
@@ -0,0 +1,8 @@
+<script src="./search-card-blank.ts" lang="ts"></script>
+<style src="./search-card-blank.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="search-card-blank">
+
+	</div>
+</template>
