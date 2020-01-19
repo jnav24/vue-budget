4f9b564537d566d36f3f4b553943f02ea3b97ac4
Justin Navarro <info@justinnavarro.net>
2020-01-19 10:21:20 -0500

    set 3 column layout

--- src/components/dashboard/lists/three-column-list/ThreeColumnList.vue
+++ src/components/dashboard/lists/three-column-list/ThreeColumnList.vue
@@ -2,7 +2,11 @@
 <style src="./three-column-list.scss" lang="scss" scoped></style>
 
 <template>
-	<div id="three-column-list">
-
+	<div id="three-column-list" class="search-list">
+		<v-layout class="list-row" :class="{'divider': showDivider }"align-center>
+			<v-flex sm5 class="col-name">{{ data.name }}</v-flex>
+			<v-flex sm3>{{ getValueFromType(data) }}</v-flex>
+			<v-flex sm4 class="col-price">{{ setCurrency(data.amount) }}</v-flex>
+		</v-layout>
 	</div>
 </template>
--- src/components/dashboard/lists/three-column-list/three-column-list.ts
+++ src/components/dashboard/lists/three-column-list/three-column-list.ts
@@ -1,6 +1,5 @@
-import { Vue, Component } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+import List from '@/components/dashboard/lists/list';
 
 @Component
-export default class ThreeColumnList extends Vue {
-
-}
+export default class ThreeColumnList extends List {}
