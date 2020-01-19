f7ececddfb8307abe1cd60fcc3c4b07d9cebb384
Justin Navarro <info@justinnavarro.net>
2020-01-18 22:55:11 -0500

    add list components

---
+++ src/components/dashboard/lists/four-column-list/FourColumnList.vue
@@ -0,0 +1,8 @@
+<script src="./four-column-list.ts" lang="ts"></script>
+<style src="./four-column-list.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="four-column-list">
+
+	</div>
+</template>
---
+++ src/components/dashboard/lists/three-column-list/ThreeColumnList.vue
@@ -0,0 +1,8 @@
+<script src="./three-column-list.ts" lang="ts"></script>
+<style src="./three-column-list.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="three-column-list">
+
+	</div>
+</template>
