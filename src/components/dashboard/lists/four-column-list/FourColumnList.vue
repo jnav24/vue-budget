668e86178456a2fcdb17f4967cce5400c988f3a3
Justin Navarro <info@justinnavarro.net>
2020-01-19 12:43:33 -0500

    add hide-row prop in order to have a row of subheads for 3 column and 4 column lists

--- src/components/dashboard/lists/four-column-list/FourColumnList.vue
+++ src/components/dashboard/lists/four-column-list/FourColumnList.vue
@@ -3,6 +3,13 @@
 
 <template>
 	<div id="four-column-list" class="search-list">
+		<v-layout class="list-row" :class="{'hide-row': hideHead }"align-center>
+			<v-flex sm5 class="col-name">Name</v-flex>
+			<v-flex sm3>{{ type === 'miscellaneous' ? '' : 'Type'}}</v-flex>
+			<v-flex sm2>Paid Date</v-flex>
+			<v-flex sm2 class="col-price">Amount Paid</v-flex>
+		</v-layout>
+
 		<v-layout class="list-row" :class="{'divider': showDivider }"align-center>
 			<v-flex sm5 class="col-name">{{ data.name }}</v-flex>
 			<v-flex sm3>{{ getValueFromType(data) }}</v-flex>
--- src/components/dashboard/lists/three-column-list/ThreeColumnList.vue
+++ src/components/dashboard/lists/three-column-list/ThreeColumnList.vue
@@ -3,6 +3,12 @@
 
 <template>
 	<div id="three-column-list" class="search-list">
+		<v-layout class="list-row" :class="{'hide-row': hideHead }"align-center>
+			<v-flex sm5 class="col-name">Name</v-flex>
+			<v-flex sm3>Type</v-flex>
+			<v-flex sm4 class="col-price">Amount Paid</v-flex>
+		</v-layout>
+
 		<v-layout class="list-row" :class="{'divider': showDivider }"align-center>
 			<v-flex sm5 class="col-name">{{ data.name }}</v-flex>
 			<v-flex sm3>{{ getValueFromType(data) }}</v-flex>
