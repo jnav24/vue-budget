1c1535d308948ba62eab2803a02283f89fe28e01
Justin Navarro <info@justinnavarro.net>
2020-01-19 13:58:11 -0500

    set data for vehicle list

--- src/components/dashboard/lists/vehicle-list/vehicle-list.ts
+++ src/components/dashboard/lists/vehicle-list/vehicle-list.ts
@@ -1,6 +1,13 @@
-import { Vue, Component } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+import List from '@/components/dashboard/lists/list';
+import {Getter} from 'vuex-class';
 
 @Component
-export default class VehicleList extends Vue {
+export default class VehicleList extends List {
+    @Getter public userVehicles: Array<{ value: string; label: string}>;
 
+    public getSelectedVehicle(id: string): string {
+        const vehicle = this.userVehicles.find((obj: { value: string; label: string}) => obj.value === id);
+        return !!vehicle && !!vehicle.label ? vehicle.label : '';
+    }
 }
