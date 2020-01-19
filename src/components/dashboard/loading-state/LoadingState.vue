4aee4a9c44bcb104e3d177223216a753619931fb
Justin Navarro <info@justinnavarro.net>
2020-01-18 21:58:41 -0500

    create loading state component

---
+++ src/components/dashboard/loading-state/LoadingState.vue
@@ -0,0 +1,16 @@
+<script src="./loading-state.ts" lang="ts"></script>
+<style src="./loading-state.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="loading-state" class="text-center">
+		<v-progress-circular
+			:size="150"
+			color="primary"
+			indeterminate></v-progress-circular>
+		<h2>Loading</h2>
+		<template v-if="showMessage">
+			<p>Hmm... This seems like it is taking forever.</p>
+			<p>Maybe refreshing the page and retrying the search could help.</p>
+		</template>
+	</div>
+</template>
