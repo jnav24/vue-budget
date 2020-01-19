182981bb077d9ea737e51345100dfee83926660e
Justin Navarro <info@justinnavarro.net>
2020-01-18 22:56:00 -0500

    inject list components in card component logic to get proper component

--- src/components/dashboard/cards/search-card/search-card.ts
+++ src/components/dashboard/cards/search-card/search-card.ts
@@ -1,7 +1,18 @@
 import {Vue, Component, Prop} from 'vue-property-decorator';
 import {currencyService, timestampService} from '@/module';
+import CreditCardList from '@/components/dashboard/lists/credit-card-list/CreditCardList.vue';
+import FourColumnList from '@/components/dashboard/lists/four-column-list/FourColumnList.vue';
+import ThreeColumnList from '@/components/dashboard/lists/three-column-list/ThreeColumnList.vue';
+import VehicleList from '@/components/dashboard/lists/vehicle-list/VehicleList.vue';
 
-@Component
+@Component({
+    components: {
+        CreditCardList,
+        FourColumnList,
+        ThreeColumnList,
+        VehicleList,
+    },
+})
 export default class SearchCard extends Vue {
     @Prop({ required: true }) public card: any;
     @Prop({ required: true }) public type: string;
@@ -14,5 +25,22 @@ export default class SearchCard extends Vue {
 
     public formatMonth(value: string): string {
         return timestampService.format(value, 'MMMM');
+    }
+
+    public getComponentName(name: string): string {
+        console.log(name);
+        if (['credit_cards'].indexOf(name) > -1) {
+            return 'CreditCardList';
+        }
+
+        if (['banks', 'medical', 'jobs', 'investments'].indexOf(name) > -1) {
+            return 'FourColumnList';
+        }
+
+        if (['vehicles'].indexOf(name) > -1) {
+            return 'VehicleList';
+        }
+
+        return 'ThreeColumnList';
     }
 }
