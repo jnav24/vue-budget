9b67b9539d58aacee4ac5210e9b56fbf279677db
Justin Navarro <info@justinnavarro.net>
2020-01-21 08:23:15 -0500

    set up summary card

--- src/components/dashboard/cards/search-card-totals/SearchCardTotals.vue
+++ src/components/dashboard/cards/search-card-totals/SearchCardTotals.vue
@@ -3,6 +3,19 @@
 
 <template>
 	<div id="search-card-totals">
-		Card totals
+		<v-layout align-center>
+			<v-flex sm6>
+				<p>Beginning Balance</p>
+				<p>Ending Balance</p>
+				<p>Lowest</p>
+				<p>Highest</p>
+				<p>Average Saved/Spent</p>
+			</v-flex>
+
+			<v-flex sm6>
+				<span>Total</span>
+				<p class="total-amount">$24,000,000.00</p>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
