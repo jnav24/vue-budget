7a7ad0351bbf48ae4e8ce63b59093b3edd999a5e
Justin Navarro <info@justinnavarro.net>
2020-02-03 13:12:52 -0500

    created verify token

---
+++ src/pages/onboard/verify/Verify.vue
@@ -0,0 +1,8 @@
+<script src="./verify.ts" lang="ts"></script>
+<style src="./verify.scss" lang="scss" scoped></style>
+
+<template>
+	<div id="verify" class="page-sub">
+		Verify yo!
+	</div>
+</template>
