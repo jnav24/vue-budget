14f4296fc9d8afffb34e4d2941daf3957a5ecd3b
Justin Navarro <info@justinnavarro.net>
2020-02-03 15:20:19 -0500

    build out the verification form

--- src/pages/onboard/verify/Verify.vue
+++ src/pages/onboard/verify/Verify.vue
@@ -3,6 +3,51 @@
 
 <template>
 	<div id="verify" class="page-sub">
-		Verify yo!
+		<v-layout justify-center>
+			<v-flex xs12 sm10 md6 lg4 xl3>
+				<v-form ref="form" v-model="formValid">
+					<v-card>
+						<v-card-title>
+							<v-flex sm2>
+								<v-icon style="font-size: 73px;">security</v-icon>
+							</v-flex>
+							<v-flex style="padding-left: 10px;text-align: left;" sm10>
+								<h1>Verify your device</h1>
+								<p>This device isn't recognized. We have sent you an email to your email address on file. For your security, enter the your verification code, from the email, in order to continue.</p>
+							</v-flex>
+						</v-card-title>
+
+						<v-alert
+							:value="alert.display"
+							transition="slide-y-transition"
+							:type="alert.type">
+							{{ alert.msg }}
+						</v-alert>
+
+						<v-card-text>
+							<v-text-field
+								v-model="form.verify.value"
+								:rules="form.verify.rules"
+								label="Enter Your Verification Code"></v-text-field>
+						</v-card-text>
+
+						<v-card-actions>
+							<v-btn
+								color="button"
+								@click="submit"
+								:disabled="!formValid"
+								block>
+								<v-progress-circular
+									:size="15"
+									color="accent"
+									v-if="loading"
+									indeterminate></v-progress-circular>
+								<span v-if="!loading">Submit</span>
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-form>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
--- src/pages/onboard/verify/verify.ts
+++ src/pages/onboard/verify/verify.ts
@@ -1,6 +1,29 @@
 import { Vue, Component } from 'vue-property-decorator';
+import {AlertInterface} from '@/interfaces/alert.interface';
+import {validateService} from '@/module';
 
 @Component
 export default class Verify extends Vue {
+    public alert: AlertInterface = {
+        type: 'error',
+        msg: '',
+        display: false,
+    };
+    public formValid = false;
+    public form: any = {
+        verify: {
+            value: '',
+            rules: [
+                (v: string) => !!v || 'Verification is required',
+                (v: string) => validateService.isValidLength(v, 6) || 'Verification code is at least 6 characters long',
+            ],
+        },
+    };
+    public loading = false;
 
+    public submit() {
+        if (this.formValid) {
+            this.loading = true;
+        }
+    }
 }
