fb128417cf400a42290d1dfc935f35038c277a5e
Justin Navarro <info@justinnavarro.net>
2020-08-10 12:34:53 -0400

    update BudgetTemplateInterface

--- src/interfaces/budget-template.interface.ts
+++ src/interfaces/budget-template.interface.ts
@@ -6,16 +6,29 @@ import {MedicalInterface} from '@/interfaces/medical.interface';
 import {MiscellaneousInterface} from '@/interfaces/miscellaneous.interface';
 import {UtilitiesInterface} from '@/interfaces/utilities.interface';
 import {VehicleInterface} from '@/interfaces/vehicle.interface';
+import {CommonExpenseTypeInterface} from '@/interfaces/common-expense-type.interface';
 
 export interface BudgetTemplateInterface {
     id?: number;
     expenses: {
         banks: BanksInterface[];
+        childcare: CommonExpenseTypeInterface[];
         credit_cards: CreditCardsInterface[];
+        education: CommonExpenseTypeInterface[];
+        entertainment: CommonExpenseTypeInterface[];
+        food: CommonExpenseTypeInterface[];
+        gift: CommonExpenseTypeInterface[];
+        housing: CommonExpenseTypeInterface[];
         investments: InvestmentsInterface[];
-        jobs: IncomesInterface[];
+        incomes: IncomesInterface[];
+        loan: CommonExpenseTypeInterface[];
         medical: MedicalInterface[];
         miscellaneous: MiscellaneousInterface[];
+        personal: CommonExpenseTypeInterface[];
+        shopping: CommonExpenseTypeInterface[];
+        subscription: CommonExpenseTypeInterface[];
+        tax: CommonExpenseTypeInterface[];
+        travel: CommonExpenseTypeInterface[];
         utilities: UtilitiesInterface[];
         vehicles: VehicleInterface[];
     };
