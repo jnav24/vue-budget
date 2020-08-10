7c4d0f9c85f02fa3c4b404bcf0bf0ec6ccbb3b16
Justin Navarro <info@justinnavarro.net>
2020-08-09 23:21:56 -0400

    fixed tslint error

--- src/components/dashboard/budgets/common-budget/common-budget.ts
+++ src/components/dashboard/budgets/common-budget/common-budget.ts
@@ -7,7 +7,7 @@ export default class CommonBudget extends Budgets {
 
     public mounted() {
         const expenseKeys = Object.keys(this.data[0]);
-        this.typeName = expenseKeys.filter(type => type.indexOf('_type_id') > -1)?.shift() ?? '';
+        this.typeName = expenseKeys.filter((type: string) => type.indexOf('_type_id') > -1)?.shift() ?? '';
         this.type = this.typeName.split('_type_id')[0];
         this.getTypeId();
     }
